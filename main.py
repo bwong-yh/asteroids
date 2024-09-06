26c26730e2bc9a1c6fb5ee5e6de2135521d344fe
Billy Wong <bwong.yh@gmail.com>
2024-09-05 21:31:50 -0600

    shoot bullets

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import sys
 import pygame
 from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
@@ -16,19 +18,21 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = updatable
     asteroid_field = AsteroidField()
 
     Player.containers = (updatable, drawable)
-
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
     player = Player(x, y)
 
+    Shot.containers = (shots, updatable, drawable)
+
     running = True
-    while running:
+    while running: 
         # check if user has closed the window
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -36,6 +40,11 @@ def main():
 
         for obj in updatable:
             obj.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print('Game over!')
+                sys.exit()
 
         screen.fill('black')
 
