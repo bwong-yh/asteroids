49c3b824ccec820c7de071fb6ac691dfab96b46a
Billy Wong <bwong.yh@gmail.com>
2024-09-05 20:03:03 -0600

    create player

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     
+    clock = pygame.time.Clock()
+    dt = 0 # second
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
 
     running = True
     while running:
@@ -14,7 +22,15 @@ def main():
                 running = False
 
         screen.fill('black')
+        player.draw(screen)
+        
         pygame.display.flip()
+
+        # pause the loop until 1/60th second has passed - ensure 60 FPS
+        # tick() returns the amount of time (ms) last frame took to rendered
+        dt = clock.tick(60) / 1000
+
+
 
 if __name__ == '__main__':
     main()
